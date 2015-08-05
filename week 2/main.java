7679948d6b811dd852692aa19727cbc775d8d11c
kaanosm <khanx5@gmail.com>
2015-08-05 13:27:14 +1000

    Week 2

---
+++ week 2/main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+
+
+
+public class main {
+
+	public static void main(String[] args) throws IOException, ClassCastException, ClassNotFoundException {
+		/* Parser ( Week 1 ) currently disabled.
+		 * 
+			Parser p = new Parser();
+			if(p.parseDir(root) != 0)
+				System.out.println("Error: parseDir");
+		 */
+
+		new Annotator();
+	}
+}
